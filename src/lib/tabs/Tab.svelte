c3de3abb861d9dac9d8c370775f513e2c79d8d2f
Hubert Lewandowski <lewandowski.hubert486@gmail.com>
2023-03-02 12:58:52 +0100

    Tabs component displaying

---
+++ src/lib/tabs/Tab.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+    import { getContext } from "svelte";
+
+    let activeTab: string = getContext('selectedTab');
+    export let title: string;
+
+    function Capitalize(str: string) {
+        return str.charAt(0).toUpperCase() + str.slice(1);
+    }
+</script>
+
+<li class="mr-2">
+    {#if activeTab === title}
+    <p
+        class="inline-block p-4 text-blue-600 border-b-2 border-blue-600 rounded-t-lg active dark:text-blue-500 dark:border-blue-500"
+        aria-current="page">{Capitalize(title)}</p>
+    {:else}
+    <p
+        class="inline-block p-4 border-b-2 border-transparent rounded-t-lg hover:text-gray-600 hover:border-gray-300 dark:hover:text-gray-300"
+        >{Capitalize(title)}</p>
+    {/if}
+</li>
---
+++ src/lib/tabs/Tabs.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+    import { setContext } from "svelte";
+
+    export let activeTab: string;
+
+    setContext('selectedTab', activeTab);
+</script>
+
+<div class="text-sm font-medium text-center text-gray-500 border-b border-gray-200 dark:text-gray-400 dark:border-gray-700">
+    <ul class="flex flex-wrap -mb-px">
+        <slot />
+    </ul>
+</div>
