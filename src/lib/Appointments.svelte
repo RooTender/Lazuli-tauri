87c70ee54a9e681e1c75855804b45e15cd26f201
Hubert Lewandowski <lewandowski.hubert486@gmail.com>
2023-03-04 23:28:56 +0100

    Add functionality to searchbar

--- src/lib/Appointments.svelte
+++ src/lib/Appointments.svelte
@@ -28,6 +28,36 @@
   let name = "";
   let greetMsg = "";
 
+  let filteredData: Array<any>;
+  let grid: any;
+  
+
+  function handleSearch(event: Event) {
+    let searchQuery = (event.target as HTMLInputElement).value;
+
+    filteredData = data.filter((row) => {
+      return Object.values(row).some((val) => {
+        return val.toString().toLowerCase().includes(searchQuery.toLowerCase());
+      });
+    });
+
+    let searchBar = document.getElementById('searchBar');
+
+    if (filteredData.length > 0)
+    {
+      grid.updateConfig({
+        data: filteredData
+      }).forceRender();
+
+      searchBar?.classList.remove('bg-red-100');
+    }
+    else 
+    {
+      searchBar?.classList.add('bg-red-100');
+    }
+    
+  }
+  
   async function greet() {
     // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
     greetMsg = await invoke("greet", { name });
@@ -40,14 +70,14 @@
       <div class="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
         <svg aria-hidden="true" class="w-5 h-5 text-gray-500 dark:text-gray-400" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path></svg>
       </div>
-      <input type="text" id="input-group-1" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg block w-full pl-10 p-2.5" placeholder="Search...">
+      <input id="searchBar" on:input={handleSearch} type="text" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg block w-full pl-10 p-2.5" placeholder="Search...">
     </div>
     <button class="bg-blue-500 hover:bg-blue-700 hover:transition-all text-white py-2 px-4 rounded-lg">
       New
     </button>
   </div>
   
-  <Grid sort {data} {pagination} search/>
+  <Grid sort {data} {pagination} bind:instance={grid} />
   
   <div class="row">
     <input id="greet-input" placeholder="Enter a name..." bind:value={name} />
