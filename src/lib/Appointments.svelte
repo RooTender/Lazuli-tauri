b31ecdeed6fdad11324acdb14622020372653dbb
Hubert Lewandowski <lewandowski.hubert486@gmail.com>
2023-03-04 22:04:16 +0100

    Add searchbar and "new" button

--- src/lib/Appointments.svelte
+++ src/lib/Appointments.svelte
@@ -11,6 +11,19 @@
 </script>
 
 <div>
+  <div class="flex flex-row space-x-2 mb-6 ">
+    <div class="relative w-full">
+      <div class="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+        <svg aria-hidden="true" class="w-5 h-5 text-gray-500 dark:text-gray-400" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path></svg>
+      </div>
+      <input type="text" id="input-group-1" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg block w-full pl-10 p-2.5" placeholder="Search...">
+    </div>
+    <button class="bg-blue-500 hover:bg-blue-700 hover:transition-all text-white py-2 px-4 rounded-lg">
+      New
+    </button>
+  </div>
+  
+  
   <div class="row">
     <input id="greet-input" placeholder="Enter a name..." bind:value={name} />
     <button on:click={greet}> Greet </button>
